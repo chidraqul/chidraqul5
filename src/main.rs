31c9a703db846fa66a39b8e0cecec58c49a20b0f
ChillerDrgon <ChillerDragon@gmail.com>
2022-04-23 11:32:41 +0200

    Add cross platform non blocking keypresses
    Using the power of stackoverflow and crossterm crate
    This helped a lot: https://stackoverflow.com/a/55201400/6287070
    
    The crosterm crate is used for cross platform keyboard input
    without the need of having to press the enter key.
    
    It is running in its own thread to not block the game tick.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+//importing in execute! macro
+extern crate crossterm;
+use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use terminal_size::{Width, Height, terminal_size};
+
 use std::process;
 use std::{thread, time};
-use terminal_size::{Width, Height, terminal_size};
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::TryRecvError;
 
 const WORLD_HEIGHT: i32 = 10;
 const WORLD_WIDTH: i32 = 20;
@@ -11,27 +19,54 @@ struct Player {
 }
 
 fn die(player: &mut Player) {
-    player.x = 0;
+    player.x = WORLD_WIDTH / 2;
     player.y = 0;
+}
+
+fn quit() {
+    disable_raw_mode().unwrap();
+	process::exit(1);
 }
 
 fn render(player: &mut Player) {
     let size = terminal_size();
     if let Some((Width(w), Height(h))) = size {
-            println!("Your terminal is {} cols wide and {} lines tall", w, h);
             let width: usize = (w - 2).into();
             for _y in 0..(h - 1) {
-                println!("|{}|", format!("{:w$}", "", w=width));
+                println!("|{}|\r", format!("{:w$}", "", w=width));
             }
     } else {
             println!("Unable to get terminal size");
-            process::exit(1);
+			quit();
     }
-    println!("x={} y={}", player.x, player.y);
+    println!("x={} y={} | ctrl+q to quit\r", player.x, player.y);
+}
+
+
+fn spawn_stdin_channel() -> Receiver<String> {
+    let (tx, rx) = mpsc::channel::<String>();
+    thread::spawn(move || loop {
+		match read().unwrap() {
+			Event::Key(KeyEvent {
+				code: KeyCode::Char('d'),
+				modifiers: KeyModifiers::NONE,
+			}) => tx.send("d".to_string()).unwrap(),
+			Event::Key(KeyEvent {
+				code: KeyCode::Char('a'),
+				modifiers: KeyModifiers::NONE,
+			}) => tx.send("a".to_string()).unwrap(),
+			Event::Key(KeyEvent {
+				code: KeyCode::Char('q'),
+				modifiers: KeyModifiers::CONTROL,
+			}) => tx.send("q".to_string()).unwrap(),
+			_ => (),
+		}
+    });
+    rx
 }
 
 fn tick(player: &mut Player) {
-    player.x += 1;
+    // player.y += 1;
     if player.x > WORLD_WIDTH || player.x < 0 {
         die(player);
     }
@@ -42,10 +77,27 @@ fn tick(player: &mut Player) {
     thread::sleep(time::Duration::from_millis(10));
 }
 
+fn got_key(key: String, player: &mut Player) {
+    if key == "q" {
+        quit();
+    } else if key == "d" {
+        player.x += 1;
+    } else if key == "a" {
+        player.x -= 1;
+    }
+}
+
 fn main() {
-    let mut player = Player { x: 0, y: 0 };
+    enable_raw_mode().unwrap();
+    let mut player = Player { x: WORLD_WIDTH / 2, y: 0 };
+    let stdin_channel = spawn_stdin_channel();
     loop {
         tick(&mut player);
+		match stdin_channel.try_recv() {
+			Ok(key) => got_key(key, &mut player),
+			Err(TryRecvError::Empty) => continue,
+			Err(TryRecvError::Disconnected) => panic!("Channel disconnected"),
+		}
     }
 }
 
